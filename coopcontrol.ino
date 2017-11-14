2177aa8496ba3847467a64085103499a70820e76
Patrick Freeman <github@e2r4.com>
2017-11-14 13:57:55 -0600

    Add support for temp, humidity, and light sensors

--- coopcontrol.ino
+++ coopcontrol.ino
@@ -1,4 +1,7 @@
 // This #include statement was automatically added by the Particle IDE.
+#include "DHT.h"
+
+// This #include statement was automatically added by the Particle IDE.
 #include <RelayShield.h>
 
 // Make our own state enumerator
@@ -10,9 +13,32 @@ RelayShield myRelays;
 // Just assume it starts off closed. Probably should be unknown or something else
 State currentState = closed;
 
+// Pins
+int light_sensor_pin = A0;
+#define DHTPIN_ONE 1
+#define DHTPIN_TWO 2
+
+// DHT parameters
+#define DHTTYPE_ONE DHT22
+#define DHTTYPE_TWO DHT22
+String DHTNAME_ONE = "inside";
+String DHTNAME_TWO = "outside";
+
+// Variables
+double one_temperature = 0;
+int one_humidity = 0;
+double two_temperature = 0;
+int two_humidity = 0;
+int light = 0;
+
+// DHT sensor
+DHT dht_one(DHTPIN_ONE, DHTTYPE_ONE);
+DHT dht_two(DHTPIN_TWO, DHTTYPE_TWO);
+
 // Millisecond of delay between relay controls
 // I saw problems with the delay being too short and the particle rebooting. (Short circuit?)
-#define DELAYMS 100
+#define RELAY_DELAY 100
+#define PUBLISH_DELAY 10000
 
 //STARTUP(WiFi.selectAntenna(ANT_INTERNAL)); // selects the CHIP antenna
 STARTUP(WiFi.selectAntenna(ANT_EXTERNAL)); // selects the u.FL antenna
@@ -21,25 +47,34 @@ STARTUP(WiFi.selectAntenna(ANT_EXTERNAL)); // selects the u.FL antenna
 void setup() {
     myRelays.begin();
     // Setup too many API function hooks
-    Particle.function("open",openDoor);
-    Particle.function("OPEN",openDoor);
-    Particle.function("close",closeDoor);
-    Particle.function("CLOSE",closeDoor);
-    Particle.function("closed",closeDoor);
-    Particle.function("CLOSED",closeDoor);
-    Particle.function("toggle",toggle);
-    Particle.function("state",state);
+    Particle.function("open", openDoor);
+    Particle.function("OPEN", openDoor);
+    Particle.function("close", closeDoor);
+    Particle.function("CLOSE", closeDoor);
+    Particle.function("closed", closeDoor);
+    Particle.function("CLOSED", closeDoor);
+    Particle.function("toggle", toggle);
+    Particle.function("state", state);
     Particle.publish("setupComplete");
+    // Variables
+    Particle.variable("light", light);
+    Particle.variable(String(DHTNAME_ONE) + "_temp", one_temperature);
+    Particle.variable(String(DHTNAME_ONE) + "_hum", one_humidity);
+    Particle.variable(String(DHTNAME_TWO) + "_temp", two_temperature);
+    Particle.variable(String(DHTNAME_TWO) + "_hum", two_humidity);
+    // Start DHT sensor
+    dht_one.begin();
+    dht_two.begin();
 }
 
 int openDoor(String args) {
     // Main control relay
-    delay(DELAYMS); myRelays.off(4);
+    delay(RELAY_DELAY); myRelays.off(4);
     // Reverse polarity
-    delay(DELAYMS); myRelays.on(2);
-    delay(DELAYMS); myRelays.on(3);
+    delay(RELAY_DELAY); myRelays.on(2);
+    delay(RELAY_DELAY); myRelays.on(3);
     // Main control relay
-    delay(DELAYMS); myRelays.on(4);
+    delay(RELAY_DELAY); myRelays.on(4);
     Particle.publish("status", "open");
     currentState = open;
     return(0);
@@ -47,12 +82,12 @@ int openDoor(String args) {
 
 int closeDoor(String args) {
     // Main control relay
-    delay(DELAYMS); myRelays.off(4);
+    delay(RELAY_DELAY); myRelays.off(4);
     // Reverse polarity
-    delay(DELAYMS); myRelays.off(3);
-    delay(DELAYMS); myRelays.off(2);
+    delay(RELAY_DELAY); myRelays.off(3);
+    delay(RELAY_DELAY); myRelays.off(2);
     // Main control relay
-    delay(DELAYMS); myRelays.on(4);
+    delay(RELAY_DELAY); myRelays.on(4);
     Particle.publish("status", "closed");
     currentState = closed;
     return(1);
@@ -78,6 +113,19 @@ int state(String args) {
 }
 
 void loop() {
-    // There is nothing to do in this main loop. Rather than doing nothing, just sleep.
-    delay(10000);
+    // Light level measurement
+    float light_measurement = analogRead(light_sensor_pin);
+    light = (int)(light_measurement/4096*100);
+    one_temperature = dht_one.getTempCelcius();
+    one_humidity = dht_one.getHumidity();
+    two_temperature = dht_two.getTempCelcius();
+    two_humidity = dht_two.getHumidity();
+
+    Spark.publish("light", String(light) + "%", PRIVATE);
+    Spark.publish(String(DHTNAME_ONE) + " temperature", String(one_temperature) + " °C", PRIVATE);
+    Spark.publish(String(DHTNAME_ONE) + " humidity", String(one_humidity) + "%", PRIVATE);
+    delay(PUBLISH_DELAY);
+    Spark.publish(String(DHTNAME_TWO) + " temperature", String(two_temperature) + " °C", PRIVATE);
+    Spark.publish(String(DHTNAME_TWO) + " humidity", String(two_humidity) + "%", PRIVATE);
+    delay(PUBLISH_DELAY);
 }
