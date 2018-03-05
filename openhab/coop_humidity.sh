e7b2aa034756876c6cd5cfd50a32d7b2eab4fa41
Pat Freeman <github@e2r4.com>
2018-03-05 01:13:45 -0600

    added sensor config and scripts

---
+++ openhab/coop_humidity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+URL="https://api.particle.io/v1/devices/${PARTICLE_DEVICE_ID}/"
+url="inside_hum"
+DATA="access_token=${PARTICLE_ACCESS_TOKEN}"
+
+ret=`curl -s ${URL}${url}?${DATA} | jq '.result'`
+if [ -n "$ret" ]; then
+	echo "$ret%"
+	exit $ret
+fi
---
+++ openhab/coop_temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+URL="https://api.particle.io/v1/devices/${PARTICLE_DEVICE_ID}/"
+url="inside_temp"
+DATA="access_token=${PARTICLE_ACCESS_TOKEN}"
+
+ret=`curl -s ${URL}${url}?${DATA} | jq '.result'`
+if [ "$ret" != "null" ]; then
+	printf "%.1f" $ret
+fi
