5dc7d12be9ca03492f36e80d9a321fee32e7a82a
Pat Freeman <github@e2r4.com>
2018-03-05 18:37:58 -0600

    prevent nulls from causing close events

--- openhab/coop_door.sh
+++ openhab/coop_door.sh
@@ -6,11 +6,13 @@ DATA="access_token=${PARTICLE_ACCESS_TOKEN}"
 function http () {
 	url=$1
 	ret=`curl -sd "${DATA}" ${URL}${url}${ARGS} | jq '.return_value'`
-	if [ "$ret" = "0" ]; then
-		echo "OPEN"
-	else
-		echo "CLOSED"
-	fi
+	case ${ret} in
+		0) echo "OPEN" ;;
+		1) echo "CLOSED" ;;
+		2) echo "OPEN_WAIT" ;;
+		3) echo "CLOSE_WAIT" ;;
+		4) echo "UNKNOWN" ;;
+	esac
 }
 
 if [ "$1" = "OPEN" -o "$1" = "CLOSED" ]; then
